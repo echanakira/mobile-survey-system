78f504edd729ae80f64b95ebf68272da2deb7485
shedninja6 <elijahchan21@gmail.com>
2019-12-01 19:29:48 -0500

    Debugging camera

--- app/src/main/java/cmsc436/mobilesurvey/utils/ScanActivity.kt
+++ app/src/main/java/cmsc436/mobilesurvey/utils/ScanActivity.kt
@@ -34,6 +34,7 @@ class ScanActivity : AppCompatActivity(){
             if (result.contents == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
             } else {
+                Toast.makeText(this, "Scanned", Toast.LENGTH_LONG).show()
                 val user = result.contents.split("&&&")[0]
                 val type = result.contents.split("&&&")[1]
                 //TODO: Return an intent with type and user
